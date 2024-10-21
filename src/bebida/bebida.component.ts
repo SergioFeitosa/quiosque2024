febc48e524fdcd9243d8ec4670edcc1b4c927cf4
Sergio Feitosa <sergiofeitosa@gmail.com>
2024-10-21 09:15:55 -0300

    criacao de componentes

---
+++ src/bebida/bebida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bebida',
+  standalone: true,
+  imports: [],
+  templateUrl: './bebida.component.html',
+  styleUrl: './bebida.component.css'
+})
+export class BebidaComponent {
+
+}
---
+++ src/caminho/caminho.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caminho',
+  standalone: true,
+  imports: [],
+  templateUrl: './caminho.component.html',
+  styleUrl: './caminho.component.css'
+})
+export class CaminhoComponent {
+
+}
